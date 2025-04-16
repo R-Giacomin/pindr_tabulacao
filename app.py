bc2d7ae43e709b6443561120d86f67250d4abaa1
rafael.giacomin <faellg@gmail.com>
2025-04-16 16:48:36 -0300

    Alteração da fonte de dados para DuckDB do repositório Painel_PNDR, mais estável que a API ArcGIS

--- app.py
+++ app.py
@@ -4,38 +4,78 @@ import pandas as pd
 import pygwalker as pyg
 from pygwalker.api.streamlit import StreamlitRenderer
 import datetime
+import requests
+import os
+import duckdb
 
 st.set_page_config(page_title="Painel de Indicadores", layout="wide")
 st.title("Tabulação para o Painel de Indicadores")
 
 @st.cache_data
-def carregar_valores_df(inicio='2020-12-31', fim='2022-12-31'):
-    gis = conectar_portal()
-    valores_item = gis.content.search("valoresmeta", item_type="Map Image Layer")[0]
-    valores_table = valores_item.tables[0]
-    valores_set = valores_table.query(
-        where=f"refdate >= DATE '{inicio}' AND refdate <= DATE '{fim}'",
-        out_fields="geoloc_id, data_name, value, refdate"
-    )
-    return pd.DataFrame([f.attributes for f in valores_set.features])
+def carregar_dados():
+    # Download the duckdb file
+    url = "https://raw.githubusercontent.com/R-Giacomin/Painel_PNDR/main/data/dados_reduzido.db"
+    file_path = "dados_reduzido.db"
 
-@st.cache_data
-def carregar_recortes_df():
-    gis = conectar_portal()
-    recortes_item = gis.content.search("recortes_geograficos", item_type="Map Image Layer")[0]
-    recortes_layer = recortes_item.layers[0]
-    valores_set = recortes_layer.query(where="1=1", out_fields="*")
-    return pd.DataFrame([f.attributes for f in valores_set.features])
+    if not os.path.exists(file_path):
+        response = requests.get(url, stream=True)
+        response.raise_for_status()  # Raise an exception for bad status codes
 
-with st.spinner("Carregando dados, esse processo pode levar alguns minutos..."):
-    valores_df = carregar_valores_df()
-    recortes_df = carregar_recortes_df()
-    df = valores_df.merge(recortes_df, left_on='geoloc_id', right_on='codigo_ibge', how='outer')
+        with open(file_path, 'wb') as file:
+            for chunk in response.iter_content(chunk_size=8192):
+                file.write(chunk)
+
+    # Connect to the database
+    con = duckdb.connect(database=file_path, read_only=True)
+
+    # Execute the query and fetch the results into a pandas DataFrame
+    query1 = "SELECT * FROM recortes_geograficos"
+    query2 = "SELECT * FROM valoresmeta"
+    recortes_geograficos = con.execute(query1).fetchdf()
+    valoresmeta = con.execute(query2).fetchdf()
+
+    # Close the connection
+    con.close()
+
+    df = valoresmeta.merge(recortes_geograficos, left_on='geoloc_id', right_on='codigo_ibge', how='left')
     df = df.dropna(subset=['codigo_ibge'])
-    df = df.drop(columns=['geoloc_id', 'OBJECTID_x', 'OBJECTID_y', 'longitude', 'latitude'])
-    df = df.rename(columns={'data_name': 'Indicador', 'value': 'Valor', 'refdate': 'Ano'})
+    df = df.drop(columns=['mdata_id',	'geoloc_id',	'local_id', 'geoloc_id', 'local_name'])
+    df = df.rename(columns={'data_name': 'Indicador','orig_name': 'Sigla_indicador' ,'value': 'Valor', 'refdate': 'Ano'})
     df = df.dropna(subset=['Ano'])
     df['Ano'] = pd.to_datetime(df['Ano']).dt.year.astype(int)
+    return df
+
+with st.spinner("Carregando dados, esse processo pode levar alguns minutos..."):
+    df = carregar_dados()
+
+#@st.cache_data
+#def carregar_valores_df(inicio='2020-12-31', fim='2022-12-31'):
+#    gis = conectar_portal()
+#    valores_item = gis.content.search("valoresmeta", item_type="Map Image Layer")[0]
+#    valores_table = valores_item.tables[0]
+#    valores_set = valores_table.query(
+#        where=f"refdate >= DATE '{inicio}' AND refdate <= DATE '{fim}'",
+#        out_fields="geoloc_id, data_name, value, refdate"
+#    )
+#    return pd.DataFrame([f.attributes for f in valores_set.features])
+
+#@st.cache_data
+#def carregar_recortes_df():
+#    gis = conectar_portal()
+#    recortes_item = gis.content.search("recortes_geograficos", item_type="Map Image Layer")[0]
+#    recortes_layer = recortes_item.layers[0]
+#    valores_set = recortes_layer.query(where="1=1", out_fields="*")
+#    return pd.DataFrame([f.attributes for f in valores_set.features])
+
+#with st.spinner("Carregando dados, esse processo pode levar alguns minutos..."):
+#    valores_df = carregar_valores_df()
+#    recortes_df = carregar_recortes_df()
+#    df = valores_df.merge(recortes_df, left_on='geoloc_id', right_on='codigo_ibge', how='outer')
+#    df = df.dropna(subset=['codigo_ibge'])
+#    df = df.drop(columns=['geoloc_id', 'OBJECTID_x', 'OBJECTID_y', 'longitude', 'latitude'])
+#    df = df.rename(columns={'data_name': 'Indicador', 'value': 'Valor', 'refdate': 'Ano'})
+#    df = df.dropna(subset=['Ano'])
+#    df['Ano'] = pd.to_datetime(df['Ano']).dt.year.astype(int)
 
 # --- Filtros ---
 st.sidebar.header("Filtros")
@@ -50,6 +90,6 @@ df_filtrado = df[
 
 # --- Exibir PyGWalker ---
 st.subheader("Explore os dados abaixo 👇")
-#vis_spec = r"""{"config":[{"config":{"defaultAggregated":true,"geoms":["bar"],"coordSystem":"generic","limit":-1,"timezoneDisplayOffset":0},"encodings":{"dimensions":[{"fid":"Indicador","name":"Indicador","basename":"Indicador","semanticType":"nominal","analyticType":"dimension","offset":0},{"fid":"Ano","name":"Ano","basename":"Ano","semanticType":"quantitative","analyticType":"dimension","offset":0},{"fid":"município","name":"município","basename":"município","semanticType":"nominal","analyticType":"dimension","offset":0},{"fid":"estado","name":"estado","basename":"estado","semanticType":"nominal","analyticType":"dimension","offset":0},{"fid":"região","name":"região","basename":"região","semanticType":"nominal","analyticType":"dimension","offset":0},{"fid":"faixa_de_fronteira","name":"faixa_de_fronteira","basename":"faixa_de_fronteira","semanticType":"nominal","analyticType":"dimension","offset":0},{"fid":"participacao_semiarido","name":"participacao_semiarido","basename":"participacao_semiarido","semanticType":"nominal","analyticType":"dimension","offset":0},{"fid":"regiao_intermediaria","name":"regiao_intermediaria","basename":"regiao_intermediaria","semanticType":"nominal","analyticType":"dimension","offset":0},{"fid":"tipologia","name":"tipologia","basename":"tipologia","semanticType":"nominal","analyticType":"dimension","offset":0},{"fid":"participacao_sudene","name":"participacao_sudene","basename":"participacao_sudene","semanticType":"nominal","analyticType":"dimension","offset":0},{"fid":"regiao_imediata","name":"regiao_imediata","basename":"regiao_imediata","semanticType":"nominal","analyticType":"dimension","offset":0},{"fid":"participacao_amazonia_legal","name":"participacao_amazonia_legal","basename":"participacao_amazonia_legal","semanticType":"nominal","analyticType":"dimension","offset":0},{"fid":"gw_mea_key_fid","name":"Measure names","analyticType":"dimension","semanticType":"nominal"}],"measures":[{"fid":"Valor","name":"Valor","basename":"Valor","analyticType":"measure","semanticType":"quantitative","aggName":"sum","offset":0},{"fid":"codigo_ibge","name":"codigo_ibge","basename":"codigo_ibge","analyticType":"measure","semanticType":"quantitative","aggName":"sum","offset":0},{"fid":"gw_count_fid","name":"Row count","analyticType":"measure","semanticType":"quantitative","aggName":"sum","computed":true,"expression":{"op":"one","params":[],"as":"gw_count_fid"}},{"fid":"gw_mea_val_fid","name":"Measure values","analyticType":"measure","semanticType":"quantitative","aggName":"sum"}],"rows":[{"fid":"município","name":"município","basename":"município","semanticType":"nominal","analyticType":"dimension","offset":0}],"columns":[{"fid":"Valor","name":"Valor","basename":"Valor","analyticType":"measure","semanticType":"quantitative","aggName":"mean","offset":0}],"color":[{"fid":"tipologia","name":"tipologia","basename":"tipologia","semanticType":"nominal","analyticType":"dimension","offset":0}],"opacity":[],"size":[],"shape":[],"radius":[],"theta":[],"longitude":[],"latitude":[],"geoId":[],"details":[],"filters":[{"fid":"Indicador","name":"Indicador","basename":"Indicador","semanticType":"nominal","analyticType":"dimension","offset":0,"rule":{"type":"one of","value":[" Percentual de escolas com acesso a esgotamento sanitário"]}},{"fid":"estado","name":"estado","basename":"estado","semanticType":"nominal","analyticType":"dimension","offset":0,"rule":{"type":"one of","value":["Rondônia"]}},{"fid":"Ano","name":"Ano","basename":"Ano","semanticType":"quantitative","analyticType":"dimension","offset":0,"rule":{"type":"not in","value":[2021,2019]}}],"text":[]},"layout":{"showActions":false,"showTableSummary":false,"stack":"stack","interactiveScale":false,"zeroScale":true,"size":{"mode":"auto","width":320,"height":200},"format":{},"geoKey":"name","resolve":{"x":false,"y":false,"color":false,"opacity":false,"shape":false,"size":false}},"visId":"gw_H9hc","name":"Chart 1"}],"chart_map":{},"workflow_list":[{"workflow":[{"type":"filter","filters":[{"fid":"Indicador","rule":{"type":"one of","value":[" Percentual de escolas com acesso a esgotamento sanitário"]}},{"fid":"estado","rule":{"type":"one of","value":["Rondônia"]}},{"fid":"Ano","rule":{"type":"not in","value":[2021,2019]}}]},{"type":"view","query":[{"op":"aggregate","groupBy":["município","tipologia"],"measures":[{"field":"Valor","agg":"mean","asFieldKey":"Valor_mean"}]}]}]}],"version":"0.4.9.15"}"""
+
 pyg_app = StreamlitRenderer(df_filtrado)
 pyg_app.explorer()
